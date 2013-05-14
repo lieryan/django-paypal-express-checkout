af1e1d3fc7baaddd1ad333c7b755b5f2f42d6fad
Lie Ryan <lie.1296@gmail.com>
2013-05-14 18:00:12 +1000

    added paypal error message to exception

--- paypal_express_checkout/exceptions.py
+++ paypal_express_checkout/exceptions.py
@@ -1,5 +1,5 @@
 class PaypalExpressException(Exception): 
     def __init__(self, response, *args, **kwargs):
         self.response = response
-        super(PaypalExpressException, self).__init__(*args, **kwargs)
+        super(PaypalExpressException, self).__init__(response.get('L_LONGMESSAGE0', ['Exception when calling Paypal'])[0], *args, **kwargs)
 
