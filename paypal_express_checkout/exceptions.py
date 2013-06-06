edf565ad143923f89362f52f77b1a8ff98584e81
Lie Ryan <lie.1296@gmail.com>
2013-06-06 16:26:25 +1000

    fixed exception not taking keyword parameter

--- paypal_express_checkout/exceptions.py
+++ paypal_express_checkout/exceptions.py
@@ -1,9 +1,9 @@
 class PaypalExpressException(Exception): 
     def __init__(self, *args, **kwargs):
         if 'response' in kwargs:
-            self.response = kwargs['response']
+            self.response = kwargs.pop('response')
             message = self.response.get('L_LONGMESSAGE0', ['Exception when calling Paypal.'])[0]
-            super(PaypalExpressException, self).__init__(message=message, *args, **kwargs)
+            super(PaypalExpressException, self).__init__(message, *args, **kwargs)
         else:
             super(PaypalExpressException, self).__init__(*args, **kwargs)
 
